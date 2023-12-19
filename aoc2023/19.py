6be7aeaa2c6153e8994a8e50ac6d455ca47004d2
Nico Bordenabe <nbordenabe@gmail.com>
2023-12-19 14:36:04 +0100

    tabs -> spaces, also updated email for commmits

--- aoc2023/19.py
+++ aoc2023/19.py
@@ -10,76 +10,75 @@ args = parser.parse_args()
 
 filepath = args.file
 with open(filepath) as f:
-	lines = [line.rstrip() for line in f]
+    lines = [line.rstrip() for line in f]
 
 empty_line_idx = lines.index('')
 conditions = dict()
 for cond in lines[:empty_line_idx]:
-	wf_name, wf_string = re.match(r'^(\w+)\{(.*)\}$', cond).groups()
-	wf_rules = wf_string.split(',')
-	rules = list()
-	for rule in wf_rules:
-		if rule.find(':') == -1:
-			rules.append([rule])
-		else:
-			rule_cond, rule_dest = rule.split(':')
-			rule_param = rule_cond[0]
-			rule_comp = rule_cond[1]
-			rule_val = int(rule_cond[2:])
-			rules.append([rule_param, rule_comp, rule_val, rule_dest])
-	conditions[wf_name] = rules
+    wf_name, wf_string = re.match(r'^(\w+)\{(.*)\}$', cond).groups()
+    wf_rules = wf_string.split(',')
+    rules = list()
+    for rule in wf_rules:
+        if rule.find(':') == -1:
+            rules.append([rule])
+        else:
+            rule_cond, rule_dest = rule.split(':')
+            rule_param = rule_cond[0]
+            rule_comp = rule_cond[1]
+            rule_val = int(rule_cond[2:])
+            rules.append([rule_param, rule_comp, rule_val, rule_dest])
+    conditions[wf_name] = rules
 
-for cond in conditions:
-	print(cond, end=' ')
-	print(conditions[cond])
+''' 
+Only for Part 2
+'''
 
 bounds = {
-	'x': [1,4000],
-	'm': [1,4000],
-	'a': [1,4000],
-	's': [1,4000],
+    'x': [1,4000],
+    'm': [1,4000],
+    'a': [1,4000],
+    's': [1,4000],
 }
 
-def calc_combinations(bounds, wf_name, depth):
-	if wf_name == 'R':
-		return 0
-	if wf_name == 'A':
-		result = 1
-		for c in 'xmas':
-			result *= (bounds[c][1]-bounds[c][0]+1)
-		return result
+def calc_combinations(bounds, wf_name):
+    if wf_name == 'R':
+        return 0
+    if wf_name == 'A':
+        result = 1
+        for c in 'xmas':
+            result *= (bounds[c][1]-bounds[c][0]+1)
+        return result
 
-	combinations = 0
-	updated_bounds = copy.deepcopy(bounds)
-	for rule in conditions[wf_name]:
-		if len(rule) == 1:
-			combinations += calc_combinations(copy.deepcopy(updated_bounds), rule[0], depth+1)
-			break
-		category, condition, value, next_wf = rule
-		if ((condition == '<' and updated_bounds[category][1] < value) or
-			(condition == '>' and updated_bounds[category][0] > value)
-		):
-			combinations += calc_combinations(copy.deepcopy(updated_bounds), next_wf, depth+1)
-			break
-		if ((condition == '<' and updated_bounds[category][0] >= value) or
-			(condition == '>' and updated_bounds[category][1] <= value)
-		):
-			continue
-		if condition == '<':
-			# updated_bounds[category][0] < value <= updated_bounds[category][1]
-			new_bounds = copy.deepcopy(updated_bounds)
-			new_bounds[category][1] = value-1
-			combinations += calc_combinations(new_bounds, next_wf, depth+1)
-			updated_bounds[category][0] = value
-		else: # rule[1] == '>'
-			# updated_bounds[category][0] <= value < updated_bounds[category][1]
-			new_bounds = copy.deepcopy(updated_bounds)
-			new_bounds[category][0] = value+1
-			combinations += calc_combinations(new_bounds, next_wf, depth+1)
-			updated_bounds[category][1] = value
-	return combinations
+    combinations = 0
+    updated_bounds = copy.deepcopy(bounds)
+    for rule in conditions[wf_name]:
+        if len(rule) == 1:
+            combinations += calc_combinations(copy.deepcopy(updated_bounds), rule[0])
+            break
+        category, condition, value, next_wf = rule
+        if ((condition == '<' and updated_bounds[category][1] < value) or
+            (condition == '>' and updated_bounds[category][0] > value)
+        ):
+            combinations += calc_combinations(copy.deepcopy(updated_bounds), next_wf)
+            break
+        if ((condition == '<' and updated_bounds[category][0] >= value) or
+            (condition == '>' and updated_bounds[category][1] <= value)
+        ):
+            continue
 
-print(calc_combinations(bounds, 'in', 0))
+        new_bounds = copy.deepcopy(updated_bounds)
+        if condition == '<':
+            new_bounds[category][1] = value-1
+            updated_bounds[category][0] = value
+        else:
+            new_bounds[category][0] = value+1
+            updated_bounds[category][1] = value
+        combinations += calc_combinations(new_bounds, next_wf)
+        
+        
+    return combinations
+
+print(calc_combinations(bounds, 'in'))
 
 ''' 
 Only for Part 1
@@ -87,33 +86,33 @@ Only for Part 1
 
 # parts = list()
 # for part_str in lines[empty_line_idx+1:]:
-# 	part_categories = part_str[1:-1].split(',')
-# 	part = {
-# 		'x': int(part_categories[0][2:]),
-# 		'm': int(part_categories[1][2:]),
-# 		'a': int(part_categories[2][2:]),
-# 		's': int(part_categories[3][2:]),
-# 	}
-# 	parts.append(part)
+#   part_categories = part_str[1:-1].split(',')
+#   part = {
+#       'x': int(part_categories[0][2:]),
+#       'm': int(part_categories[1][2:]),
+#       'a': int(part_categories[2][2:]),
+#       's': int(part_categories[3][2:]),
+#   }
+#   parts.append(part)
 
 # def follow_wf(part, wf_name):
-# 	if wf_name in ['A', 'R']:
-# 		return wf_name
-# 	for rule in conditions[wf_name]:
-# 		if len(rule) == 1:
-# 			return follow_wf(part, rule[0])
-# 		if ((rule[1] == '<' and part[rule[0]] < rule[2]) or
-# 			(rule[1] == '>' and part[rule[0]] > rule[2])
-# 		):
-# 			return follow_wf(part, rule[3])
+#   if wf_name in ['A', 'R']:
+#       return wf_name
+#   for rule in conditions[wf_name]:
+#       if len(rule) == 1:
+#           return follow_wf(part, rule[0])
+#       if ((rule[1] == '<' and part[rule[0]] < rule[2]) or
+#           (rule[1] == '>' and part[rule[0]] > rule[2])
+#       ):
+#           return follow_wf(part, rule[3])
 
 # total = 0
 # for part in parts:
-# 	verdict = follow_wf(part, 'in')
-# 	print(part, end=' ')
-# 	print(verdict)
-# 	if verdict == 'A':
-# 		total += part['x']+part['m']+part['a']+part['s']
+#   verdict = follow_wf(part, 'in')
+#   print(part, end=' ')
+#   print(verdict)
+#   if verdict == 'A':
+#       total += part['x']+part['m']+part['a']+part['s']
 
 # print(total)
 
